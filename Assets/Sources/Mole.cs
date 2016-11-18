3757555b19af57dd5b7ab7d0787fc7c0470c3e40
Lucas Wall <lucas.wall@lemonchiligames.com>
2016-11-17 23:03:59 -0300

    Initial commit.

---
+++ Assets/Sources/Mole.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mole : MonoBehaviour {
+
+	public float moveDistance;
+	public float moveTime;
+	public float waitTime;
+	public TextMesh letterLabel;
+	public float shakeAmplitude;
+	public float scaleTo;
+
+	public char Letter { get { return letterLabel.text[0]; } }
+
+	float startY;
+	float wait = 0.0f;
+	float dir = 0.0f;
+
+	void Start() {
+		startY = transform.position.y;
+	}
+
+	public void Activate() {
+		if ( gameObject.activeSelf ) {
+			Debug.LogWarning("Mole double activation!");
+		}
+		gameObject.SetActive(true);
+		dir = 1.0f;
+		wait = 0.0f;
+	}
+
+	public void SetLetter(char letter) {
+		letterLabel.text = letter.ToString();
+	}
+
+	public void Hit() {
+		MoveDown();
+		StartCoroutine(HitEffect());
+	}
+
+	public void Miss() {
+		MoveDown();
+		StartCoroutine(MissEffect());
+	}
+
+	void Update() {
+		if ( wait > 0.0f ) {
+			wait -= Time.deltaTime;
+		} else {
+			transform.Translate(Vector3.up * dir * (moveDistance/moveTime) * Time.deltaTime);
+			if ( dir > 0.0f && transform.position.y >= startY + moveDistance ) {
+				MoveDown();
+			} else if ( dir < 0.0f && transform.position.y <= startY ) {
+				gameObject.SetActive(false);
+			}
+		}
+	}
+
+	void MoveDown() {
+		wait = waitTime;
+		dir = -1.0f;
+	}
+
+	IEnumerator HitEffect() {
+		float t = 0.0f;
+		while ( t <= 1.0f ) {
+			t += Time.deltaTime / waitTime;
+			transform.rotation = Quaternion.Euler(0.0f, 0.0f, Random.Range(-shakeAmplitude, shakeAmplitude));
+			yield return null;
+		}
+		transform.rotation = Quaternion.identity;
+	}
+
+	IEnumerator MissEffect() {
+		float t = 0.0f;
+		Vector3 scaleStart = transform.localScale;
+		Vector3 scaleEnd = scaleStart * scaleTo;
+		while ( t <= 1.0f ) {
+			t += Time.deltaTime / (waitTime/2.0f);
+			transform.localScale = Vector3.Lerp(scaleStart, scaleEnd, t);
+			yield return null;
+		}
+		t = 0.0f;
+		while ( t <= 1.0f ) {
+			t += Time.deltaTime / (waitTime/2.0f);
+			transform.localScale = Vector3.Lerp(scaleEnd, scaleStart, t);
+			yield return null;
+		}
+	}
+
+}
---
+++ Assets/Sources/MoleMaster.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MoleMaster : MonoBehaviour {
+
+	public int minActivate;
+	public int maxActivate;
+	public float roundPauseTime;
+	public Text letterLabel;
+
+	Mole[] moles;
+	float roundPause;
+	char currentLetter;
+	bool roundActive = false;
+
+	const string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	void Start() {
+		roundPause = roundPauseTime;
+		moles = FindObjectsOfType<Mole>();
+		Debug.Log("Found " + moles.Length + " moles.");
+		for ( int i = 0; i < moles.Length; i++ ) {
+			moles[i].gameObject.SetActive(false);
+		}
+	}
+
+	void Update() {
+		CheckForRoundActivation();
+		if ( roundActive ) {
+			CheckForMoleHit();
+		}
+	}
+
+	void CheckForRoundActivation() {
+		if ( roundPause <= 0.0f && ! MolesActive() ) {
+			roundPause = roundPauseTime;
+		}
+		if ( roundPause > 0.0f ) {
+			roundPause -= Time.deltaTime;
+			if ( roundPause <= 0.0f ) {
+				ActivateMoles();
+			}
+		}
+	}
+
+	void CheckForMoleHit() {
+		if ( Input.GetMouseButtonDown(0) ) {
+			Mole mole = MoleUnderMouse();
+			if ( mole != null ) {
+				Debug.Log("Hit!");
+				roundActive = false;
+				if ( mole.Letter == currentLetter ) {
+					mole.Hit();
+				} else {
+					mole.Miss();
+				}
+			}
+		}
+	}
+
+	Mole MoleUnderMouse() {
+		RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+		return (hit.collider != null) ? hit.collider.GetComponent<Mole>() : null;
+	}
+
+	void ActivateMoles() {
+		Debug.Log("Go moles!");
+		currentLetter = RandomLetter(' ');
+		letterLabel.text = currentLetter.ToString();
+		List<Mole> hat = new List<Mole>(moles);
+		bool first = true;
+		for ( int count = Random.Range(minActivate, maxActivate+1);
+				count > 0; count-- ) {
+			Mole m = hat[Random.Range(0, hat.Count)];
+			hat.Remove(m);
+			if ( first ) m.SetLetter(currentLetter);
+			else m.SetLetter(RandomLetter(currentLetter));
+			first = false;
+			m.Activate();
+		}
+		roundActive = true;
+	}
+
+	char RandomLetter(char exclude) {
+		char c;
+		do {
+			c = letters[Random.Range(0, letters.Length)];
+		} while ( c == exclude );
+		return c;
+	}
+
+	bool MolesActive() {
+		for ( int i = 0; i < moles.Length; i++ ) {
+			if ( moles[i].gameObject.activeSelf ) return true;
+		}
+		return false;
+	}
+
+}
